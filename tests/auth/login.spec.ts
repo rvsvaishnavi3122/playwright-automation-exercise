1865d7ac69ae5a8dddfb13d82228115014998cec
rvsvaishnavi3122 <rvsvaishnavi3122@gmail.com>
2026-03-11 21:56:17 +0530

    login test is modified

--- tests/auth/login.spec.ts
+++ tests/auth/login.spec.ts
@@ -1,23 +1,14 @@
-import { test, expect,chromium } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { LoginPage } from '../../pages/LoginPage';
 import { testuser, invalidUser } from '../../fixtures/test-data';
-//import { chromium, test } from '@playwright/test';
 
-test('launch browser manually', async () => {
-  const browser = await chromium.launch({ headless: true });
-  const page = await browser.newPage();
-  await page.goto('https://automationexercise.com');
-  await browser.close();
-});
 test('Test case 2: Login user with correct email and password', async ({ page }) => {
   const loginPage = new LoginPage(page);
 
   await loginPage.goToLogin();
   await loginPage.login(testuser.email, testuser.password);
 
-  await expect(
-    page.getByText('Logged in as')
-  ).toBeVisible();
+  await expect(page.getByText('Logged in as')).toBeVisible();
 });
 
 test('Test case 3: Login user with incorrect creds', async ({ page }) => {
@@ -41,9 +32,7 @@ test('Test case 4: Logout user', async ({ page }) => {
 
   await loginPage.logout();
 
-  await expect(
-    page.getByText('Login to your account')
-  ).toBeVisible();
+  await expect(page.getByText('Login to your account')).toBeVisible();
 });
 
 test('Test case 5:Register with existing email', async ({ page }) => {
